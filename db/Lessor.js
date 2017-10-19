27e0d68393c7c472d29ff48e96d4e2d9c5a58de1
Bertrand <bertrandpenguilly@laposte.net>
2017-10-19 16:07:28 +0200

    Changed the DB schema a bit (Lessor + Client => User), added a insertion test on the DB (it works! ...for now)

--- db/Lessor.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var LessorSchema = new Schema({
-  name: String,
-  coord_long: Number,
-  coord_lat: Number
-});
-module.exports = mongoose.model("Lessor", LessorSchema);
--- db/db.js
+++ db/db.js
@@ -1,9 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var Client = require('./Client.js');
-var Contract = require('./Contract.js');
-var Lessor = require('./Lessor.js');
-var Offer = require('./Offer.js');
-var ShoeBrand = require('./ShoeBrand.js');
-var ShoeModel = require('./ShoeModel.js');
+var dbModels = {};
+
+dbModels.Contract = require('./Contract.js');
+dbModels.Offer = require('./Offer.js');
+dbModels.ShoeBrand = require('./ShoeBrand.js');
+dbModels.ShoeModel = require('./ShoeModel.js');
+dbModels.User = require('./User.js');
+
+exports.dbModels = dbModels;
