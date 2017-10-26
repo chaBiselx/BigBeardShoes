cae3d02d9ae63aab13474e9786c0764b733233b9
Bertrand <bertrandpenguilly@laposte.net>
2017-10-26 14:44:58 +0200

    fix the previous merge

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -6,17 +6,14 @@ const ShoeBrand = require('../models/ShoeBrand.js');
 const ShoeModel = require('../models/ShoeModel.js');
 // TODO currently only list the models, not the actual shoes
 
-
-
-exports.addShoes = (req, res) => {
-  Shoes.find((err, docs) => {
-    res.render('sellshoes', { sellshoes: docs });
+exports.getShoes = (req, res) => {
+  ShoeModel.find({}).populate('brand').exec(function(err, docs) {
+    res.render('shoes', { shoes: docs });
   });
 };
 
-
-exports.getShoes = (req, res) => {
-  Shoes.find((err, docs) => {
+exports.addShoes = (req, res) => {
+  ShoeModel.find({}).populate('brand').exec(function(err, docs) {
     res.render('shoes', { shoes: docs });
   });
 };
