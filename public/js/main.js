5a91455cc505ee4242df1c0fb94d6ecc545b09fb
Charles BISELX <cha.biselx@gmail.com>
2017-11-02 14:28:35 +0100

    fill form to purchase

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,6 @@
 
 function getBuy(){
 
-  var popup = window.open("./buy" , "Commander", "width=800,height=500");// , "toolbar" = no );//, "location" = no , "menubar" = no );
+  var popup = window.open("./buy" , "Commander", "width=800,height=500");
 
 }
